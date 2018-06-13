9794c5bd4aa5ed38bd2232602527a353fdd0c933
Keionne Derousselle <knnderousselle@gmail.com>
2018-06-13 01:53:14 -0600

    Adding some content to the page

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,53 @@
 <template>
   <div id="app">
     <app-header></app-header>
+    <div class="container body-content">
+      <div class="section">
+        <h3>Skills</h3>
+      </div>
+      <div class="section">
+        <h3>Experience</h3>
+      </div>
+      <div class="section">
+        <h3>Education</h3>
+      </div>
+      <div class="section">
+        <h3>Awards</h3>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
-  import Header from './components/shared/Header.vue';
+  import Header from "./components/shared/Header.vue";
 
   export default {
     components: {
       appHeader: Header
     }
-  }
+  };
 </script>
 
 <style lang="scss">
-  html, body, #app {
+  html,
+  body,
+  #app {
     height: 100%;
     width: 100%;
+    background-color: #e7e7e4;
+    font-family: "Open Sans", "Helvetica Neue", Helvetica, Arial, sans-serif;
+    font-size: 16px;
+    line-height: 24px;
+    color: #1b1b18;
+  }
+
+  .body-content {
+    padding: 40px 0px;
+  }
+
+  .section h3{
+    text-transform: uppercase;
+    letter-spacing: 3px;
+    font-size: 1.25rem;
   }
 </style>
--- src/components/shared/Header.vue
+++ src/components/shared/Header.vue
@@ -1,8 +1,11 @@
 <template>
   <header>
     <div class="header-parallax">
-      <div class="header-title">
-        <h1>Keionne Derousselle</h1>
+      <div class="container">
+        <div class="header-title">
+          <h1>Keionne Derousselle</h1>
+          <h2>Software Developer</h2>
+        </div>
       </div>
     </div>
   </header>
@@ -12,28 +15,36 @@
   header {
     height: 100%;
   }
+
   .header-parallax {
     height: 100%;
-    background-image: url('../../assets/headerImage.jpg');
+    background-image: url("../../assets/headerImage.jpg");
     background-repeat: no-repeat;
-    background-size: auto 100%;
-    background-position: top;
+    background-size: cover;
+    background-position: center;
     background-attachment: fixed;
   }
 
   .header-title {
-    display: flex;
-    align-items: center;
-    justify-content: center;
-    text-align: center;
     height: inherit;
+    font-family: "Raleway", "Helvetica Neue", Helvetica, Arial, sans-serif;
+    color: #1b1b18;
+    text-transform: uppercase;
+    padding-top: 160px;
   }
 
-  h1 {
-    font-family: "Raleway", "Helvetica Neue", Helvetica, Arial, sans-serif;
-    color: #fff;
-    text-transform: uppercase;
+  .header-title h1,
+  .header-title h2 {
+    font-weight: 700;
+  }
+
+  .header-title h1 {
     letter-spacing: 6px;
+  }
+
+  .header-title h2 {
+    font-weight: 700;
+    letter-spacing: 3px;
   }
 </style>
 
