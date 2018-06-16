210feee7ef558e7688c2bad40cdf175c9586a931
Keionne Derousselle <knnderousselle@gmail.com>
2018-06-16 00:44:11 -0600

    adding content to education and awards sections

--- src/components/awards/Awards.vue
+++ src/components/awards/Awards.vue
@@ -1,5 +1,9 @@
 <template>
   <app-section section-title="Awards" id="awards">
+    <h4>Neumont Merit Scholarship</h4>
+    <h4>President’s List</h4>
+    <h4>Dean’s List</h4>
+    <h4>Featured Student</h4>
 
   </app-section>
 </template>
--- src/components/education/Education.vue
+++ src/components/education/Education.vue
@@ -1,6 +1,20 @@
 <template>
   <app-section section-title="Education" id="education">
-
+    <div class="education-icon">
+      <i class="material-icons md-48">school</i>
+    </div>
+    <div class="row">
+      <div class="col-md">
+        <h4>Neumont University, Salt Lake City, UT</h4>
+        <h4>Bachelor of Science in Computer Science</h4>
+        <h4>Graduated cum laude with a GPA of 3.54 out of a possible 4.0</h4>
+      </div>
+      <div class="col-md">
+        <h4>Louisiana School for Math, Science, and the Arts, Nachitoches, LA</h4>
+        <h4>High School Diploma</h4>
+        <h4>Graduated with a GPA of 3.2 out of a possible 4.0</h4>
+      </div>
+    </div>
   </app-section>
 </template>
 
@@ -14,5 +28,9 @@
   };
 </script>
 
-<style>
+<style lang="scss" scoped>
+  .education-icon {
+    color: #7d9389;
+    text-align: center;
+  }
 </style>
