d4240824b5454e3cbc6666cb73f23d3b2fd99b65
Keionne Derousselle <knnderousselle@gmail.com>
2018-06-17 03:07:41 -0600

    starting to create prod webpack config

--- webpack.common.js
+++ webpack.common.js
@@ -1,31 +1,19 @@
 var path = require('path');
 var webpack = require('webpack');
-
-function resolve(dir) {
-  return path.join(__dirname, '..', dir);
-};
-
-const createLintingRule = () => ({
-  test: /\.(js|vue)$/,
-  loader: 'eslint-loader',
-  enforce: 'pre',
-  include: [resolve('src'), resolve('test')],
-  options: {
-    formatter: require('eslint-friendly-formatter'),
-    emitWarning: false
-  }
-});
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+// const CleanWebpackPlugin = require('clean-webpack-plugin');
+// const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const VueLoaderPlugin = require('vue-loader/lib/plugin');
 
 module.exports = {
   entry: './src/main.js',
   output: {
-    path: path.resolve(__dirname, './dist'),
+    path: path.resolve(__dirname, 'dist'),
     publicPath: '/dist/',
     filename: 'build.js'
   },
   module: {
     rules: [
-      ...[createLintingRule()],
       {
         test: /\.css$/,
         use: [
@@ -54,9 +42,6 @@ module.exports = {
         loader: 'vue-loader',
         options: {
           loaders: {
-            // Since sass-loader (weirdly) has SCSS as its default parse mode, we map
-            // the "scss" and "sass" values for the lang attribute to the right configs here.
-            // other preprocessors should work out of the box, no loader config like this necessary.
             'scss': [
               'vue-style-loader',
               'css-loader',
@@ -68,7 +53,6 @@ module.exports = {
               'sass-loader?indentedSyntax'
             ]
           }
-          // other vue-loader options go here
         }
       },
       {
@@ -91,34 +75,16 @@ module.exports = {
     },
     extensions: ['*', '.js', '.vue', '.json']
   },
-  devServer: {
-    historyApiFallback: true,
-    noInfo: true,
-    overlay: true
-  },
-  performance: {
-    hints: false
-  },
-  devtool: '#eval-source-map'
-}
 
-if (process.env.NODE_ENV === 'production') {
-  module.exports.devtool = '#source-map'
-  // http://vue-loader.vuejs.org/en/workflow/production.html
-  module.exports.plugins = (module.exports.plugins || []).concat([
-    new webpack.DefinePlugin({
-      'process.env': {
-        NODE_ENV: '"production"'
+  plugins: [
+    new VueLoaderPlugin(),
+    new CopyWebpackPlugin([
+      {
+        from: 'index.html',
+        to: 'index.html'
       }
-    }),
-    new webpack.optimize.UglifyJsPlugin({
-      sourceMap: true,
-      compress: {
-        warnings: false
-      }
-    }),
-    new webpack.LoaderOptionsPlugin({
-      minimize: true
-    })
-  ])
-}
+    ]),
+    // new CleanWebpackPlugin(['dist']),
+    // new MiniCssExtractPlugin('styles.[contentHash].css'),
+  ]
+};
---
+++ webpack.config.dev.js
@@ -0,0 +1,44 @@
+const webpack = require('webpack');
+const merge = require('webpack-merge');
+const common = require('./webpack.common.js');
+var path = require('path');
+
+function resolve(dir) {
+  return path.join(__dirname, '..', dir);
+};
+
+const createLintingRule = () => ({
+  test: /\.(js|vue)$/,
+  loader: 'eslint-loader',
+  enforce: 'pre',
+  include: [resolve('src'), resolve('test')],
+  options: {
+    formatter: require('eslint-friendly-formatter'),
+    emitWarning: false
+  }
+});
+
+
+module.exports = merge(common, {
+  devServer: {
+    contentBase: './dist',
+    publicPath: '/dist/',
+    hot: true
+  },
+
+  performance: {
+    hints: false
+  },
+
+  devtool: '#eval-source-map',
+
+  module: {
+    rules: [
+      ...[createLintingRule()]
+    ],
+  },
+
+  plugins: [
+    new webpack.DefinePlugin({'process.env.NODE_ENV': JSON.stringify('development')})
+  ]
+});
